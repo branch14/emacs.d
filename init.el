26d68e821695e34f52d077424150bdd7bd2a1aca
phil <phil@branch14.org>
2018-03-04 22:22:44 +0100

    make init.el perfect :)

--- init.el
+++ init.el
@@ -2,12 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
+;;; Defining this as an interactive function means that it can be
+;;; called with `M-x load-config` anytime to reload the configuration.
 (defun load-config ()
+  "Load the literate configuration from `config.org`."
   (interactive)
   (org-babel-load-file "~/.emacs.d/config.org"))
 
 (load-config)
 
 (provide 'init)
-
 ;;; init.el ends here
