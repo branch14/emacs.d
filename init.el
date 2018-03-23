81dbd95a42f4d78a5fa3117faab93d5044091402
phil <phil@branch14.org>
2018-03-23 15:13:16 +0100

    even more perfect init.el

--- init.el
+++ init.el
@@ -2,14 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
-;;; Defining this as an interactive function means that it can be
-;;; called with `M-x load-config` anytime to reload the configuration.
 (defun load-config ()
   "Load the literate configuration from `config.org`."
-  (interactive)
   (org-babel-load-file "~/.emacs.d/config.org"))
 
+;; load the configuration on initial startup
 (load-config)
+
+(defconst save-triggers-reload-config '("init.el" "config.org"))
+
+(defun reload-config ()
+  "Reload config when the saved file is a config file."
+  (let ((filename (file-name-nondirectory buffer-file-name)))
+    (if (member filename save-triggers-reload-config)
+	(load-config))))
+
+(add-hook 'after-save-hook 'reload-config)
 
 (provide 'init)
 ;;; init.el ends here
